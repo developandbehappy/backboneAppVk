14197db56b58e2b81001d847f721b9228b9f01e0
zidein07 <zidein07@gmail.com>
2015-12-22 21:40:14 +0400

    refactor gomno cod

--- src/modules/template.js
+++ src/modules/template.js
@@ -2,39 +2,15 @@ var app = app || {};
 var Backbone = Backbone || {};
 
 app.tpl = {
-  postListTpl: function (context) {
-    var source = $("#postListTpl").html();
+  homeTpl: function (context) {
+    return this.getTpl(context, '#homeTpl');
+  },
+  authByTokenTpl: function (context) {
+    return this.getTpl(context, '#authByTokenTpl');
+  },
+  getTpl: function (context, selector) {
+    var source = $(selector).html();
     var template = Handlebars.compile(source);
     return template(context);
-  },
-  postListItemTpl: function (context) {
-    var source = $("#postListItemTpl").html();
-    var template = Handlebars.compile(source);
-    return template(context);
-  },
-  postItemTpl: function (context) {
-    var source = $("#postItemTpl").html();
-    var template = Handlebars.compile(source);
-    return template(context);
-  },
-  postItemComment: function (context) {
-    var source = $("#postItemComment").html();
-    var template = Handlebars.compile(source);
-    return template(context);
-  },
-  userData: function (context) {
-    var source = $("#userData").html();
-    var template = Handlebars.compile(source);
-    return template(context);
-  },
-  userComments: function (context) {
-    var source = $("#userComments").html();
-    var template = Handlebars.compile(source);
-    return template(context);
-  },
-  userSubmitted: function (context) {
-    var source = $("#userSubmitted").html();
-    var template = Handlebars.compile(source);
-    return template(context);
-  },
+  }
 };
