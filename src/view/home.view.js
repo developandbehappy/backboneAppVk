2fa71bb09b5157c3016104f1cf677cdf52ad00fd
zidein07 <zidein07@gmail.com>
2015-12-23 11:55:31 +0400

    sort by page

--- src/view/home.view.js
+++ src/view/home.view.js
@@ -7,26 +7,23 @@ app.home = Backbone.View.extend({
 
   },
   render: function () {
-    var self = this;
     this.token = app.token.getToken();
     this.fetchUserData().then(function (response) {
       console.log('response', response);
       var html = app.tpl.homeTpl({
-        avatarUrl: response.photo_max,
-        firstName: response.first_name,
-        lastName: response.last_name,
-        city: response.city,
-        country: response.country,
-        followers_count: response.followers_count,
-        has_mobile: response.has_mobile,
-        last_seen: self.getDateLastVisit(response.last_seen),
-        online: self.getOnlineById(response.online),
-        sex: response.sex,
+        online: app.dataByUser.getOnlineById(response.online),
         status: response.status,
         uid: response.uid,
         bdate: response.bdate,
-        sex: self.getSexById(response.sex),
-
+        sex: app.dataByUser.getSexById(response.sex),
+        country: response.country,
+        city: response.city,
+        has_mobile: response.has_mobile,
+        last_seen: app.dataByUser.getDateLastVisit(response.last_seen),
+        avatarUrl: response.photo_max,
+        firstName: response.first_name,
+        lastName: response.last_name,
+        followers_count: response.followers_count
       });
       $('.content').html(html);
     });
@@ -39,24 +36,5 @@ app.home = Backbone.View.extend({
       console.log('response', response);
       return response.response[0];
     });
-  },
-  getSexById: function (sex) {
-    if (sex === 0) {
-      return 'Пользователь не указал пол';
-    } else if (sex === 1) {
-      return 'Женский';
-    } else {
-      return 'Мужской'
-    }
-  },
-  getOnlineById: function (onlineId) {
-    if (onlineId === 0) {
-      return 'Оффлайн';
-    } else if (onlineId === 1) {
-      return 'Онлайн';
-    }
-  },
-  getDateLastVisit: function (lastVisit) {
-    return new Date(lastVisit.time).getUTCDate();
   }
 });
