da87a04ce31b9f42163e874c43b94cf0be0de32b
AveVlad <dev@vld.me>
2015-12-26 23:24:03 +0300

    add component

---
+++ src/view/profile/profile.photo.view.js
@@ -0,0 +1,9 @@
+var app = app || {};
+var Backbone = Backbone || {};
+
+app.home = Backbone.View.extend({
+  el: '.container',
+  render: function () {
+    console.log("RENDER_PHOTO_COMPONENT");
+  }
+});
